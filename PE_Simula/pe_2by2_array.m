2316ed0f249cc5a28e8d2bf5eb542f296798766b
20jacksond <dlj3@rice.edu>
2023-10-25 09:13:48 -0500

    Changed inputs to edge PEs to simplify overall layer structure. Added bias (and update) to occur by neuron rather than in an individual bias PE.

--- PE_Simula/pe_2by2_array.m
+++ PE_Simula/pe_2by2_array.m
@@ -4,7 +4,7 @@
 % This function has to come first according to Matlab convention
 % All other functions in this file are used in this main array function
 
-function [output_k, delta_kmin1, weight_changes, bias_changes] = pe_array(weights, biases, output_kmin1, partial_sum_delta_k, delta_k, partial_sum_out_k, eta, model, alpha)
+function [output_k, delta_kmin1, weight_changes, bias_changes] = pe_array(weights, biases, output_kmin1, delta_k, eta, model, alpha)
     % inputs: 
     % weights - array representing all the weights for the 2x2 array
     % output_kmin1 - previous layer output array for each neuron
@@ -29,30 +29,36 @@ function [output_k, delta_kmin1, weight_changes, bias_changes] = pe_array(weight
     bias_changes = zeros(2, 1);
     
     % first weight PE, first neuron
-    sum_delta_out11, sum_output_out11, weight_changes(1, 1) = weights_pe(delta_k(1), output_kmin1(1), partial_sum_out_k(1), partial_sum_delta_k(1), init_weight11, eta);
+    sum_delta_out11, sum_output_out11, weight_changes(1, 1) = weights_pe(delta_k(1), output_kmin1(1), init_bias1, 0, init_weight11, eta);
     
     % second weight PE, first neuron
-    sum_delta_out12, sum_output_out12, weight_changes(1, 2) = weights_pe(delta_k(1), output_kmin1(2), sum_output_out11, partial_sum_delta_k(2), init_weight12, eta);
+    sum_delta_out12, net_sum1, weight_changes(1, 2) = weights_pe(delta_k(1), output_kmin1(2), sum_output_out11, 0, init_weight12, eta);
     
     % output of the first neuron
-    net_sum1, bias_changes(1) = bias_pe(delta_k(1), sum_output_out12, init_bias1, eta);
     output_k(1) = act_pe(net_sum1, model, alpha);
     
     %----------------------------------------------------------------------
     
     % first weight PE, second neuron
-    sum_delta_out21, sum_output_out21, weight_changes(2, 1) = weights_pe(delta_k(2), output_kmin1(1), partial_sum_out_k(1), sum_delta_out11, init_weight21, eta);
+    sum_delta_out21, sum_output_out21, weight_changes(2, 1) = weights_pe(delta_k(2), output_kmin1(1), init_bias2, sum_delta_out11, init_weight21, eta);
     
     % second weight PE, second neuron
-    sum_delta_out22, sum_output_out22, weight_changes(2, 2) = weights_pe(delta_k(2), output_kmin1(2), sum_output_out21, sum_delta_out12, init_weight22, eta);
+    sum_delta_out22, net_sum2, weight_changes(2, 2) = weights_pe(delta_k(2), output_kmin1(2), sum_output_out21, sum_delta_out12, init_weight22, eta);
     
     % output of the second neuron
-    net_sum2, bias_changes(2) = bias_pe(delta_k(2), sum_output_out22, init_bias2, eta);
     output_k(2) = act_pe(net_sum2, model, alpha);
     
     % update the backpropagation outputs
     delta_kmin1(1) = sum_delta_out21;
     delta_kmin1(2) = sum_delta_out22;
+    
+    % update to the bias of neuron 1
+    k1 = delta_k(1);
+    bias_changes(1) = k1*eta - init_bias1;
+    
+    % update to the bias of neuron 2
+    k2 = delta_k(2);
+    bias_changes(2) = k2*eta - init_bias2;
     
 end
 %% Processing Element Components
@@ -79,19 +85,19 @@ end
 
 % Updates to the biases
 
-function [net_sum, bias_change] = bias_pe(delta_k, sum_in, init_bias, eta)
-    seq_len = size(delta_k);
-    bias = init_bias;
-    net_sum = zeros(seq_len, 1);
-    bias_change = zeros(seq_len, 1);
-    for i = 1:seq_len
-        sum = sum_in(i);
-        k = delta_k(i);
-        net_sum(i) = bias + sum;
-        bias = k*eta - bias;
-        bias_change(i) = bias;
-    end
-end
+% function [net_sum, bias_change] = bias_pe(delta_k, sum_in, init_bias, eta)
+%     seq_len = size(delta_k);
+%     bias = init_bias;
+%     net_sum = zeros(seq_len, 1);
+%     bias_change = zeros(seq_len, 1);
+%     for i = 1:seq_len
+%         sum = sum_in(i);
+%         k = delta_k(i);
+%         net_sum(i) = bias + sum;
+%         bias = k*eta - bias;
+%         bias_change(i) = bias;
+%     end
+% end
 
 % Applying the Activation Function (ReLU, ELU, and Sigmoid)
 function output = act_pe(net_in, model, alpha)
